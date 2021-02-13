a1383f99745130aa4664ca8c5f1027d884176e2c
purisquiri <mau.dorigo@outlook.es>
2021-02-13 14:01:06 -0500

    sign up centered, after sign up home page show all the practitioners, about page fixed

--- frontend/index.js
+++ frontend/index.js
@@ -1,60 +1,60 @@
+const url = "http://localhost:3000/practitioners";
+const urlUP = "http://localhost:3000/user_practitioners";
+const urlUser = "http://localhost:3000/user";
 
-const url = "http://localhost:3000/practitioners"
-const urlUP = "http://localhost:3000/user_practitioners"
-const urlUser = "http://localhost:3000/user"
+const main = document.querySelector("#main");
 
-const main = document.querySelector("#main")
+document.addEventListener("DOMContentLoaded", () => {
+  getPract();
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    getPract()
+  // document.querySelector('form').addEventListener('submit', createLogin)
+  document.querySelector("#aboutBtn").addEventListener("click", (event) => {
+    renderAbout(event);
+  });
+});
 
-    // document.querySelector('form').addEventListener('submit', createLogin)
-    document.querySelector('#aboutBtn').addEventListener('click', (event) => {
-        renderAbout(event)
-    })
-
- })
-
-function getPract(){
-fetch(url)
-    .then(resp => resp.json())
-    .then(practArray => practArray.forEach(practitioner => renderP(practitioner)))
+function getPract() {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((practArray) =>
+      practArray.forEach((practitioner) => renderP(practitioner))
+    );
 }
-function renderP(practitioner){
-    const main = document.querySelector("#main")
-    let card = document.createElement("div")
-    main.appendChild(card)
-    card.className = "ui card"
-    card.id = practitioner.id
+function renderP(practitioner) {
+  const main = document.querySelector("#main");
+  let card = document.createElement("div");
+  main.appendChild(card);
+  card.className = "ui card";
+  card.id = practitioner.id;
 
-    let allUsers = practitioner.user_practitioners
+  let allUsers = practitioner.user_practitioners;
 
-    function likesCounter(totalLikes, pract) {
-        return totalLikes + pract.likes;
+  function likesCounter(totalLikes, pract) {
+    return totalLikes + pract.likes;
+  }
+
+  function commentCounter(totalComents, pract) {
+    let nComments = 0;
+    if (pract.reviews.length > 0) {
+      nComments += 1;
+
+      return totalComents + nComments;
     }
+    // function incrementValue()
+    // {
+    //     var value = parseInt(document.getElementById('number').value, 10);
+    //     value = isNaN(value) ? 0 : value;
+    //     value++;
+    //     document.getElementById('number').value = value;
+    // }
+    // onclick to hearts??
+  }
 
-    function commentCounter(totalComents, pract) {
-        let nComments = 0
-        if (pract.reviews.length > 0){
-            nComments += 1
-           
-            return totalComents + nComments
-        }
-        // function incrementValue()
-        // {
-        //     var value = parseInt(document.getElementById('number').value, 10);
-        //     value = isNaN(value) ? 0 : value;
-        //     value++;
-        //     document.getElementById('number').value = value;
-        // }
-        // onclick to hearts??
-        
-     }
-    
-
-    card.innerHTML = `
+  card.innerHTML = `
             <div class="content">
-            <a id="cardName" data-id=${practitioner.id} class="header">${practitioner.name}</a>
+            <a id="cardName" data-id=${practitioner.id} class="header">${
+    practitioner.name
+  }</a>
             </div>
             <div class="image">
                 <img src = "${practitioner.image}">
@@ -70,172 +70,156 @@ function renderP(practitioner){
             <div class="extra content" >
                 Specialties: ${practitioner.specialties}
             </div>
-            `
-        
-    card.querySelector(".header").addEventListener("click", (event) => {
-       showPract(event, practitioner)})
+            `;
 
-   
+  card.querySelector(".header").addEventListener("click", (event) => {
+    showPract(event, practitioner);
+  });
 }
 
-function showPract(event, practitioner){
-    // console.log(event.target.dataset.id);
-    document.querySelector("#main").innerHTML = ""
-    
-    let card = document.createElement('div')
-    card.className = "ui centered card"
+function showPract(event, practitioner) {
+  // console.log(event.target.dataset.id);
+  document.querySelector("#main").innerHTML = "";
 
-    let name = document.createElement('div')
-    name.className = "centered"
-    name.id="nameText"
-    name.innerText = practitioner.name
+  let card = document.createElement("div");
+  card.className = "ui centered card";
 
-    let img = document.createElement('img')
-    img.src = practitioner.image
+  let name = document.createElement("div");
+  name.className = "centered";
+  name.id = "nameText";
+  name.innerText = practitioner.name;
 
-    let allUsers = practitioner.user_practitioners
+  let img = document.createElement("img");
+  img.src = practitioner.image;
 
-    function likesCounter(totalLikes, pract) {
-        return totalLikes + pract.likes;
-    }
+  let allUsers = practitioner.user_practitioners;
 
-    let likesDiv = document.createElement('div')
-    likesDiv.className = "content"
-    let upLikes = document.createElement('span')
-    upLikes.className = "right floated"
-    let heart = document.createElement('i')
-    heart.className = "heart outline like icon"
-    heart.textContent = allUsers.reduce(likesCounter, 0)
+  function likesCounter(totalLikes, pract) {
+    return totalLikes + pract.likes;
+  }
 
-    
-    let reviewSegment = document.createElement('div')
-    reviewSegment.className = "ui secondary segment"
-    reviewSegment.id="reviewText"
-    reviewSegment.innerText = "Reviews: "
-    
-    let reviewDiv = document.createElement('ul')
-    reviewDiv.id="reviewList"
-    let array = practitioner.user_practitioners.map(function(userReview) {
-        
-        return userReview.reviews}).forEach(review => { 
-            reviewDiv.innerHTML += `<li>${review}</li>`
-        })
+  let likesDiv = document.createElement("div");
+  likesDiv.className = "content";
+  let upLikes = document.createElement("span");
+  upLikes.className = "right floated";
+  let heart = document.createElement("i");
+  heart.className = "heart outline like icon";
+  heart.textContent = allUsers.reduce(likesCounter, 0);
 
-    let commentsForm = document.createElement('form')
-    commentsForm.className = "content"
-    commentsForm.id = practitioner.id
-    
-    let placeHolder = document.createElement('div')
-    placeHolder.className = "ui large transparent left icon input"
-    let inputForm = document.createElement('input')
-    inputForm.type ="text"
-    inputForm.name ="review"
-    inputForm.id ="review-input"
-    inputForm.placeholder= "Add Review..."
-    // inputForm.value = ""
-    let button = document.createElement('button')
-    button.className = "ui primary button"
-    button.id = "submit"
-    button.innerText = "submit a review"
-   
+  let reviewSegment = document.createElement("div");
+  reviewSegment.className = "ui secondary segment";
+  reviewSegment.id = "reviewText";
+  reviewSegment.innerText = "Reviews: ";
 
-    
-    let segment = document.createElement('div')
-    segment.className = "ui raised segment"
-    segment.id="segmentText"
-    // segment.style.width = "650px"
-    let about = document.createElement('p')
-    about.innerText = practitioner.about
-    let specialtiesTitle = document.createElement('ul')
-    specialtiesTitle.innerText = 'Specialties:'
-    let specialties = document.createElement('li')
-    practitioner.specialties.forEach(specialty => {
-    specialties.innerHTML += `<li>${specialty}</li>`})
-    let languages = document.createElement('p')
-    languages.innerText = `Languages Spoken: ${practitioner.languages}`
-    let zipCode = document.createElement('p')
-    zipCode.innerText = `Zip Code: ${practitioner.zip_code}`
-
-    heart.addEventListener("click", () => {
-        incrementLikes(event, heart)
+  let reviewDiv = document.createElement("ul");
+  reviewDiv.id = "reviewList";
+  let array = practitioner.user_practitioners
+    .map(function (userReview) {
+      return userReview.reviews;
     })
-    
-    
-    placeHolder.append(inputForm)
-    commentsForm.append(placeHolder, button)
-    
-    reviewSegment.append(reviewDiv, commentsForm)
-    
-    specialtiesTitle.append(specialties)
-    segment.append(about, specialtiesTitle, languages, zipCode)
+    .forEach((review) => {
+      reviewDiv.innerHTML += `<li>${review}</li>`;
+    });
 
-    upLikes.append(heart)
-    likesDiv.append(upLikes)
-    card.append(name, img, likesDiv)
+  let commentsForm = document.createElement("form");
+  commentsForm.className = "content";
+  commentsForm.id = practitioner.id;
 
+  let placeHolder = document.createElement("div");
+  placeHolder.className = "ui large transparent left icon input";
+  let inputForm = document.createElement("input");
+  inputForm.type = "text";
+  inputForm.name = "review";
+  inputForm.id = "review-input";
+  inputForm.placeholder = "Add Review...";
+  // inputForm.value = ""
+  let button = document.createElement("button");
+  button.className = "ui primary button";
+  button.id = "submit";
+  button.innerText = "submit a review";
 
-    main.append(card, segment, reviewSegment)
+  let segment = document.createElement("div");
+  segment.className = "ui raised segment";
+  segment.id = "segmentText";
+  // segment.style.width = "650px"
+  let about = document.createElement("p");
+  about.innerText = practitioner.about;
+  let specialtiesTitle = document.createElement("ul");
+  specialtiesTitle.innerText = "Specialties:";
+  let specialties = document.createElement("li");
+  practitioner.specialties.forEach((specialty) => {
+    specialties.innerHTML += `<li>${specialty}</li>`;
+  });
+  let languages = document.createElement("p");
+  languages.innerText = `Languages Spoken: ${practitioner.languages}`;
+  let zipCode = document.createElement("p");
+  zipCode.innerText = `Zip Code: ${practitioner.zip_code}`;
 
-    commentsForm.addEventListener("submit", function(event){
-        event.preventDefault();
-        postReview(event, commentsForm, reviewDiv, heart)
-    })
+  heart.addEventListener("click", () => {
+    incrementLikes(event, heart);
+  });
 
+  placeHolder.append(inputForm);
+  commentsForm.append(placeHolder, button);
 
+  reviewSegment.append(reviewDiv, commentsForm);
 
+  specialtiesTitle.append(specialties);
+  segment.append(about, specialtiesTitle, languages, zipCode);
+
+  upLikes.append(heart);
+  likesDiv.append(upLikes);
+  card.append(name, img, likesDiv);
+
+  main.append(card, segment, reviewSegment);
+
+  commentsForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    postReview(event, commentsForm, reviewDiv, heart);
+  });
 }
 
 function incrementLikes(event, heart) {
-    //let plus = +event.target.textContent + 1
-    //heart.textContent = plus
-    let addLike = +heart.textContent + 1
-    let newHeartLikes = heart.textContent = addLike
-
-    
-
-
+  //let plus = +event.target.textContent + 1
+  //heart.textContent = plus
+  let addLike = +heart.textContent + 1;
+  let newHeartLikes = (heart.textContent = addLike);
 }
 
+function postReview(event, commentsForm, reviewDiv, heart) {
+  event.preventDefault();
+  let practitionerId = commentsForm.id;
 
+  let newLikes = heart.innerText;
 
-function postReview(event, commentsForm, reviewDiv, heart){
-    event.preventDefault();
-    let practitionerId = commentsForm.id
+  let userId = document.getElementById("userDiv");
 
-    let newLikes = heart.innerText 
+  let newReviewText = commentsForm.review.value;
 
-    let userId = document.getElementById("userDiv")
-    
-    let newReviewText = commentsForm.review.value
-    
-    let li = document.createElement('li')
-    li.innerText = commentsForm.review.value
-    reviewDiv.appendChild(li)
-    console.log(newReviewText);
-    commentsForm.reset();
-    
-   
-    let newReview= {
-        user_id: +userId.dataset.userId,
-        practitioner_id: +practitionerId,
-        reviews: newReviewText,
-        likes: +newLikes
-    }
+  let li = document.createElement("li");
+  li.innerText = commentsForm.review.value;
+  reviewDiv.appendChild(li);
+  console.log(newReviewText);
+  commentsForm.reset();
 
-    console.log();
-    
-    fetch(urlUP, {
-        method: "POST",
-        headers:{
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        
-        body: JSON.stringify(newReview)
-    }).then(response => response.json())
-    .then(newdata => console.log(newdata)) 
+  let newReview = {
+    user_id: +userId.dataset.userId,
+    practitioner_id: +practitionerId,
+    reviews: newReviewText,
+    likes: +newLikes,
+  };
 
+  console.log();
 
+  fetch(urlUP, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+
+    body: JSON.stringify(newReview),
+  })
+    .then((response) => response.json())
+    .then((newdata) => console.log(newdata));
 }
-
-
--- frontend/user.js
+++ frontend/user.js
@@ -1,49 +1,39 @@
-const userURL= "http://localhost:3000/users"
+const userURL = "http://localhost:3000/users";
 
-document.addEventListener('DOMContentLoaded', () => {
-    // loginForm()
-    // let signForm = document.querySelector('form')
-    let signUpBtn = document.querySelector('#signUpBtn')
+document.addEventListener("DOMContentLoaded", () => {
+  // loginForm()
+  // let signForm = document.querySelector('form')
+  let signUpBtn = document.querySelector("#signUpBtn");
 
+  signUpBtn.addEventListener("click", (event) => {
+    renderForm(event);
+  });
+});
 
-    signUpBtn.addEventListener('click', (event) => {
-        renderForm(event)
-        
-    })
+function renderForm(event) {
+  document.querySelector("main").innerHTML = "";
 
-   
-})
+  let form = document.createElement("form");
+  form.className = "container card ui form justify-content-center";
 
-function renderForm(event){
-    document.querySelector('main').innerHTML = ""
-
-    let form= document.createElement('form')
-        form.className="ui form"
-    
-     
-    form.innerHTML=  
-    
-    `   <h3>Sign Up</h3>
+  form.innerHTML = `   <h3 class="card-title text-center">Sign Up</h3><br/>
         <input type="text" placeholder="Name" name="name">
-         <input type="text" placeholder="Email" name="email">
-         <button type="submit" class="ui button" name="submit">Submit</button>`
+         <input type="text" placeholder="Email" name="email"><br/>
+         <button type="submit" class="ui button" name="submit">Submit</button>`;
 
-         document.querySelector('#main').appendChild(form)
+  document.querySelector("#main").appendChild(form);
 
-         document.querySelector('form').addEventListener('submit', (event) =>{
-            createUser(event)
-         })
+  document.querySelector("form").addEventListener("submit", (event) => {
+    createUser(event);
+  });
 }
-
-
 
 //     let loginBtn= document.querySelector('#loginBtn')
 
 //     loginBtn.addEventListener('click', () => {
 //         let formSection= document.querySelector('.form')
-       
-         
-//         formSection.innerHTML=  
+
+//         formSection.innerHTML=
 
 //         `  <h3>Login</h3>
 //              <input type="text" placeholder="Email" name="email">
@@ -59,53 +49,49 @@ function renderForm(event){
 //     let userInfo= {
 //         email: event.target.email
 //     }
-    
-    
+
 //     fetch('http://localhost:3000/login', {
-        
+
 //         method: "POST",
 //         headers:{
 //             "Content-Type": "application/json",
 //             "Accept": "application/json"
 //         },
-        
+
 //         body: JSON.stringify(userInfo)
 //     }).then(response => response.json())
 //     .then(data => data)
-//     document.querySelector('form').innerHTML= ""  
+//     document.querySelector('form').innerHTML= ""
 //     console.log("you're user email is"+ `${userInfo.email}`)
 //     }
-    
-    
-    
-    function createUser(event) {
-        event.preventDefault()
-        let name = event.target.name.value
-        document.getElementById("userDiv").innerText = `Hi ${name}!`
-        
-        let user = document.getElementById("userDiv")
-        // userId.dataset
-        let newUser= {
-            name: name,
-            email: event.target.email.value
-        }
-        
-        fetch(userURL, {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            
-            body: JSON.stringify(newUser)
-        }).then(response => response.json())
-        .then(data => { 
-            data,
-            user.dataset.userId = data.id
-            getPract()
-            
-        })
-        
-        document.querySelector('form').innerHTML= `<h3 class="welcome">Welcome!</h3>
-                    <img class="ui circular image" src="https://i.imgur.com/eiJ32tN.jpg" width= "750px">`
-    }
+
+function createUser(event) {
+  event.preventDefault();
+  let name = event.target.name.value;
+  document.getElementById("userDiv").innerText = `Hi ${name}!`;
+
+  let user = document.getElementById("userDiv");
+  // userId.dataset
+  let newUser = {
+    name: name,
+    email: event.target.email.value,
+  };
+
+  fetch(userURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+
+    body: JSON.stringify(newUser),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data, (user.dataset.userId = data.id);
+      getPract();
+    });
+  document.querySelector("main").innerHTML = "";
+  //document.querySelector("form").innerHTML = `<h3 class="welcome">Welcome!</h3>
+  // <img class="ui circular image" src="https://i.imgur.com/eiJ32tN.jpg" width= "750px">`;
+}
